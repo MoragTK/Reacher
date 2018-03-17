15c6b14818e28e515651c8aa04a1bec0ac2cde58
Morag <morag.tohamy@campus.technion.ac.il>
2018-03-18 01:16:01 +0200

    first comments

--- DataBase.py
+++ DataBase.py
@@ -1,29 +1,40 @@
 from collections import deque
 import numpy as np
+
+
 class DataBase():
-    def __init__(self,N=10):
-        self.size=N
-        self.Q=deque(maxlen=N) # xn=6,un=2,xn1=6
+
+    # Constructor
+    def __init__(self, N = 10):
+        self.size = N
+        self.Q = deque(maxlen=N) #xn = 6, un = 2, xn1 = 6
         return
-    def append(self,xu=None,xn1=None):
-        self.Q.append(np.hstack((xu,xn1)))
+
+    # receives Xk, Uk, and Xk+1, and adds them as another data sample to the data base.
+    def append(self, xu=None, xn1=None):
+        self.Q.append(np.hstack((xu, xn1))) #TODO: Doesnt need to be [xu, xn1]?
         return
-    def getLast(self):
-        xn=self.Q.pop()
+
+    # returns the newest sample at the data base
+    def getLast(self): #TODO: I dont think it needs "self" inside... does it?
+        xn = self.Q.pop()
         self.Q.append(xn)
-        tmpX=xn[0,8:]   # xn1
-        tmpU=xn[0,6:8]  #un  we assume the next u will be close to the previos
+        tmpX=xn[0,8:]   # xn1 #TODO: Xk+1?
+        tmpU=xn[0,6:8]  # un. We assume the next u will be close to the previous #TODO: Where is the original Xk?
         r=np.hstack((tmpX,tmpU))
         return r
-    def getAll(self):   # this is for training
-        input=np.zeros((self.size,8))
-        target=np.zeros((self.size,6))
-        for i in range(0,self.size):
-            tmpOut=self.Q.pop()
-            input[i,:]=tmpOut[0,:8]
-            target[i,:]=tmpOut[0,8:]
-        for i in range(0,self.size):
-            tmpIn=np.hstack((input[i,:],target[i,:]))
-            tmpIn=np.matrix(tmpIn)
+
+    # returns all of the data in the data base.  ???
+    def getAll(self):   # this is for training #TODO: Do we need to send self?
+        input = np.zeros((self.size, 8))
+        target = np.zeros((self.size, 6))
+        for i in range(0, self.size):
+            tmpOut = self.Q.pop()
+            input[i, :] = tmpOut[0, :8]
+            target[i, :] = tmpOut[0, 8:]
+        for i in range(0, self.size):
+            tmpIn = np.hstack((input[i,:],target[i,:]))
+            tmpIn = np.matrix(tmpIn)
             self.Q.appendleft(tmpIn)
         return [input,target]
+        #TODO: Question... Is this the cycle thing?
